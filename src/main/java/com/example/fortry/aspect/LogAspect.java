af4ebf98fa8c4522e645cf4d6a8f5fd925984c48
ysk <1991919895@qq.com>
2026-08-22 11:45:01 +0800

    加入aop

---
+++ src/main/java/com/example/fortry/aspect/LogAspect.java
@@ -0,0 +1,36 @@
+package com.example.fortry.aspect;
+
+import jakarta.servlet.ServletRequest;
+import org.apache.juli.logging.LogFactory;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+
+@Aspect
+@Component
+public class LogAspect {
+    private static final Logger log=  LoggerFactory.getLogger(LogAspect.class);
+
+    @Pointcut("execution(* com.example.fortry.controller..*.*(..))")
+    public void controllerLog(){}
+
+    @Around("controllerLog()")
+   public Object logAround(ProceedingJoinPoint JoinPoint) throws Throwable{
+
+        long start=System.currentTimeMillis();
+        log.info("\n============================"+start+ "开始 ==============================");
+
+        Object object=JoinPoint.proceed();
+
+        long end=System.currentTimeMillis();
+        log.info("\n===================="+end+" 结束，耗时："+(end-start)+"====================");
+
+        return object;
+    }
+}
